ecee8e536dba87528435cd801fefa2c3fa12246f
Vitaliy Stepanenko <mail@vitaliy.in>
2015-05-05 16:11:33 +0300

    getValues & getWrieable methods added, refactoring

--- src/Manipulator.php
+++ src/Manipulator.php
@@ -1,7 +1,9 @@
 <?php
 namespace Nayjest\Manipulator;
 
+use Exception;
 use ReflectionClass;
+use RuntimeException;
 
 /**
  * Class Manipulator
@@ -69,20 +71,15 @@ class Manipulator
      */
     public static function assignBySetters($instance, array $fields)
     {
-        $assigned_properties = [];
+        $assignedProperties = [];
         foreach ($fields as $key => $value) {
-            $camel_case_name = str_replace(
-                ' ',
-                '',
-                ucwords(str_replace(array('-', '_'), ' ', $key))
-            );
-            $method_name = 'set' . $camel_case_name;
-            if (method_exists($instance, $method_name)) {
-                $instance->$method_name($value);
-                $assigned_properties[] = $key;
+            $methodName = 'set' . self::snakeToCamelCase($key);
+            if (method_exists($instance, $methodName)) {
+                $instance->$methodName($value);
+                $assignedProperties[] = $key;
             }
         }
-        return $assigned_properties;
+        return $assignedProperties;
     }
 
     /**
@@ -100,5 +97,110 @@ class Manipulator
             $assigned_fields,
             self::assignBySetters($instance, $fields)
         );
+    }
+
+    protected static function snakeToCamelCase($str)
+    {
+        return str_replace(
+            ' ',
+            '',
+            ucwords(str_replace(array('-', '_'), ' ', $str))
+        );
+    }
+
+    protected static function camelToSnakeCase($str)
+    {
+        $str = lcfirst($str);
+        $lowerCase = strtolower($str);
+        $result = '';
+        $length = strlen($str);
+        for ($i = 0; $i < $length; $i++) {
+            $result .= ($str[$i] === $lowerCase[$i] ? '' : '_') . $lowerCase[$i];
+        }
+        return $result;
+    }
+
+    protected static $writable = [];
+
+    /**
+     * Returns names of writable properties.
+     *
+     * @param object|string|array $target
+     * @param bool $useSetters
+     * @return array
+     */
+    public static function getWritable($target, $useSetters = true)
+    {
+        if (is_array($target)) {
+            return array_keys($target);
+        }
+        $class = is_object($target) ? get_class($target) : $target;
+        $cacheKey = $class . ($useSetters?'+s':'');
+        if (!array_key_exists($cacheKey, self::$writable)) {
+            self::$writable[$cacheKey] = array_keys(get_class_vars($class));
+            if ($useSetters) {
+                $setters = self::getSetters($class);
+                foreach ($setters as $setter) {
+                    self::$writable[$cacheKey][] = self::camelToSnakeCase(substr($setter, 3));
+                }
+            }
+        }
+        return self::$writable[$cacheKey];
+    }
+
+    protected static function getXMethods($key, $obj)
+    {
+        $res = [];
+        $methods = get_class_methods($obj);
+        foreach ($methods as $method) {
+            if (strpos($method, $key) === 0 && strlen($method) > 3 and ctype_upper($method{3})) {
+                $res[] = $method;
+            }
+        }
+        return $res;
+    }
+
+    public static function getSetters($obj)
+    {
+        return self::getXMethods('set', $obj);
+    }
+
+    public static function getGetters($obj)
+    {
+        return self::getXMethods('get', $obj);
+    }
+
+    /**
+     * Returns values of properties specified in $propNames argument.
+     *
+     * @experimental
+     *
+     * @param object|array $obj
+     * @param string[] $propNames
+     * @return array
+     */
+    public static function getValues($obj, array $propNames)
+    {
+        $values = [];
+        $isArray = is_array($obj);
+        foreach ($propNames as $key) {
+            if ($isArray) {
+                if (array_key_exists($key, $obj)) {
+                    $values[$key] = $obj[$key];
+                }
+            } else {
+                if (property_exists($obj, $key)) {
+                    $values[$key] = $obj->{$key};
+                }
+            }
+        }
+        foreach ($propNames as $key) {
+            if (array_key_exists($key, $values)) continue;
+            $getter = 'get' . self::snakeToCamelCase($key);
+            if (method_exists($obj, $getter)) {
+                $values[$key] = $obj->{$getter}();
+            }
+        }
+        return $values;
     }
 }
